15f6eded1f499b4bb8968a0cd9d9cab2cd636b53
Dmitry T <trimonovds@gmail.com>
2016-05-07 00:24:34 +0300

    new version

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-﻿sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+﻿sudo /etc/init.d/nginx start
+ps -o pid,euser,egroup,comm,args -C nginx
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
