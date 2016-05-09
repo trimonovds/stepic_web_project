bbe183e87c241fa3d5ad364c8dfaa561b431a529
Dmitry T <trimonovds@gmail.com>
2016-05-09 23:27:56 +0300

    Actually first understood version

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,12 @@
-import multiprocessing
-
-bind = "127.0.0.1:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web',
+    # 'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--daemon',
+        '--workers=2',
+        '--timeout=60',
+        'hello:app',
+    ),
+}
