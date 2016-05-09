d5c17afec700cf66d6cc34c95d946da6cbbf775d
Dmitry T <trimonovds@gmail.com>
2016-05-09 23:33:55 +0300

    changed wsgi code

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
-import urlparse
-
+#!/usr/bin/python
 def app(environ, start_response):
-	params = urlparse.parse_qs(environ["QUERY_STRING"])
-
-	start_response("200 OK", [("Content-Type", "text/plain")])
-
-	return ['%s=%s<br>' % (k, params[k][0]) for k in params]
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [data]
