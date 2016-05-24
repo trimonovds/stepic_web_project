b2cc6c4b4f4e8a09f5c701277262444f43ee50c3
Dmitry T <trimonovds@gmail.com>
2016-05-24 03:29:54 +0300

    to first task version
    With understanding

--- hello.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-def app(environ, start_response):
-    data = ''
-    for line in environ["QUERY_STRING"].split("&"):
-        data = data+line+"\n"
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [data]
