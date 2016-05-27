6e162b0c344d655de5ddd3f7a1e77363b44bda15
Dmitry T <trimonovds@gmail.com>
2016-05-27 22:05:33 +0400

    added real stepic nginx.conf + init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 ﻿#!/usr/bin/env bash
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+cd /home/box/web/
+sudo gunicorn --config='/etc/gunicorn.d/hello.py' hello:app
