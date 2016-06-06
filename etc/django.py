bf09d95993944059b6e8b85e5600612af0b57898
Dmitry T <trimonovds@gmail.com>
2016-06-07 00:32:45 +0300

    some changes

--- etc/django.py
+++ etc/django.py
@@ -1,2 +1,12 @@
-pythonpath = '/home/box/web'
-bind = "0.0.0.0:8000"
+CONFIG = {
+  'mode': 'wsgi',
+  'python': '/usr/bin/python3',
+  'working_dir': '/home/box/web/ask',
+  'args': (
+    '--bind=0.0.0.0:8000',
+    '--workers=2',
+    '--timeout=15',
+    '--log-level=debug',
+    'ask.wsgi:application',
+  ),
+}
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-﻿#!/usr/bin/env bash
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+﻿sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-
-gunicorn -c /home/box/web/etc/hello.py hello:app &
-gunicorn -c /home/box/web/etc/django.py wsgi &
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
