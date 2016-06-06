15f69620388c6782841752c0376288474ad4db3e
Dmitry T <trimonovds@gmail.c>
2016-06-07 00:00:58 +0300

    correct version i think

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 ﻿#!/usr/bin/env bash
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-cd /home/box/web/
-sudo gunicorn --config='/etc/gunicorn.d/hello.py' hello:app &
-cd ask/
-sudo gunicorn -b :8000 ask.wsgi:application &
+
+gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
+gunicorn -c /home/box/web/etc/django.py wsgi --daemon
