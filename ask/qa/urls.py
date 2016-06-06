4ebea60b1712e6f2a23c1b689282c901ea8552ba
Dmitry T <trimonovds@gmail.com>
2016-06-06 23:49:55 +0300

    first try module 2 task 1

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^login/$', test),
     url(r'^signup/$', test),
     url(r'^question/(?P<question_id>[0-9]+)/$', test),
-    url(r'^ask/$', test),
+    url(r'^ask/.*$', test),
     url(r'^popular/$', test),
     url(r'^new/$', test),
 ]
